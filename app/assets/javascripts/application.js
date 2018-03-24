64d0274c7256ca7837f02a9ae37d7a28cdfba624
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-03-24 16:55:06 -0700

    removes bootstrap, jquery, and xirsys dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require popper
-//= require bootstrap
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
---
+++ app/assets/javascripts/signaling-server.js
@@ -0,0 +1,189 @@
+// Broadcast Types
+const JOIN_ROOM = "JOIN_ROOM";
+const EXCHANGE = "EXCHANGE";
+const REMOVE_USER = "REMOVE_USER";
+
+// DOM Elements
+let currentUser;
+let localVideo;
+let remoteVideoContainer;
+
+// Objects
+let pcPeers = {};
+let localstream;
+
+window.onload = () => {
+  currentUser = document.getElementById("current-user").innerHTML;
+  localVideo = document.getElementById("local-video");
+  remoteVideoContainer = document.getElementById("remote-video-container");
+};
+
+// Ice Credentials
+const ice = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
+
+// Initialize user's own video
+document.onreadystatechange = () => {
+  if (document.readyState === "interactive") {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: true
+      })
+      .then(stream => {
+        localstream = stream;
+        localVideo.srcObject = stream;
+        localVideo.muted = true;
+      })
+      .catch(logError);
+  }
+};
+
+const handleJoinSession = async () => {
+  App.session = await App.cable.subscriptions.create("SessionChannel", {
+    connected: () => {
+      broadcastData({
+        type: JOIN_ROOM,
+        from: currentUser
+      });
+    },
+    received: data => {
+      console.log("received", data);
+      if (data.from === currentUser) return;
+      switch (data.type) {
+        case JOIN_ROOM:
+          return joinRoom(data);
+        case EXCHANGE:
+          if (data.to !== currentUser) return;
+          return exchange(data);
+        case REMOVE_USER:
+          return removeUser(data);
+        default:
+          return;
+      }
+    }
+  });
+};
+
+const handleLeaveSession = () => {
+  for (user in pcPeers) {
+    pcPeers[user].close();
+  }
+  pcPeers = {};
+
+  App.session.unsubscribe();
+
+  remoteVideoContainer.innerHTML = "";
+
+  broadcastData({
+    type: REMOVE_USER,
+    from: currentUser
+  });
+};
+
+const joinRoom = data => {
+  createPC(data.from, true);
+};
+
+const removeUser = data => {
+  console.log("removing user", data.from);
+  let video = document.getElementById(`remoteVideoContainer+${data.from}`);
+  video && video.remove();
+  delete pcPeers[data.from];
+};
+
+const createPC = (userId, isOffer) => {
+  let pc = new RTCPeerConnection(ice);
+  pcPeers[userId] = pc;
+  pc.addStream(localstream);
+
+  isOffer &&
+    pc
+      .createOffer()
+      .then(offer => {
+        pc.setLocalDescription(offer);
+        broadcastData({
+          type: EXCHANGE,
+          from: currentUser,
+          to: userId,
+          sdp: JSON.stringify(pc.localDescription)
+        });
+      })
+      .catch(logError);
+
+  pc.onicecandidate = event => {
+    event.candidate &&
+      broadcastData({
+        type: EXCHANGE,
+        from: currentUser,
+        to: userId,
+        candidate: JSON.stringify(event.candidate)
+      });
+  };
+
+  pc.onaddstream = event => {
+    const element = document.createElement("video");
+    element.id = `remoteVideoContainer+${userId}`;
+    element.autoplay = "autoplay";
+    element.srcObject = event.stream;
+    remoteVideoContainer.appendChild(element);
+  };
+
+  pc.oniceconnectionstatechange = event => {
+    if (pc.iceConnectionState == "disconnected") {
+      console.log("Disconnected:", userId);
+      broadcastData({
+        type: REMOVE_USER,
+        from: userId
+      });
+    }
+  };
+
+  return pc;
+};
+
+const exchange = data => {
+  let pc;
+
+  if (!pcPeers[data.from]) {
+    pc = createPC(data.from, false);
+  } else {
+    pc = pcPeers[data.from];
+  }
+
+  if (data.candidate) {
+    pc
+      .addIceCandidate(new RTCIceCandidate(JSON.parse(data.candidate)))
+      .then(() => console.log("Ice candidate added"))
+      .catch(logError);
+  }
+
+  if (data.sdp) {
+    sdp = JSON.parse(data.sdp);
+    pc
+      .setRemoteDescription(new RTCSessionDescription(sdp))
+      .then(() => {
+        if (sdp.type === "offer") {
+          pc.createAnswer().then(answer => {
+            pc.setLocalDescription(answer);
+            broadcastData({
+              type: EXCHANGE,
+              from: currentUser,
+              to: data.from,
+              sdp: JSON.stringify(pc.localDescription)
+            });
+          });
+        }
+      })
+      .catch(logError);
+  }
+};
+
+const broadcastData = data => {
+  fetch("sessions", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "content-type": "application/json" }
+  });
+};
+
+const logError = error => console.warn("Whoops! Error:", error);
