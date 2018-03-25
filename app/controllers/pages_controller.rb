3bd8cf57a96523048d1f9b0b7769365c30f90be4
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2018-03-24 17:54:02 -0700

    removes unused code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,5 @@
-require 'rest-client'
-require 'json'
-
 class PagesController < ApplicationController
   def home
     @random_number = rand(0...10_000)
-
-    response = RestClient.put ENV['GET_XIRSYS_ICE'], accept: :json
-    @json_response = response.to_json
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,12 @@
 class SessionsController < ApplicationController
   def create
     head :no_content
-    ActionCable.server.broadcast "session_channel", params
+    ActionCable.server.broadcast 'session_channel', session_params
+  end
+
+  private
+
+  def session_params
+    params.permit(:type, :from, :to, :sdp, :candidate)
   end
 end
