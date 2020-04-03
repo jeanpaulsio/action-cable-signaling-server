24f25164917cf2e3987933ee2b97b1bbd04c7617
JP Sio <sio.jeanpaul@gmail.com>
2020-04-02 21:23:21 -0700

    Run app:update

--- config/application.rb
+++ config/application.rb
@@ -11,15 +11,9 @@ module ActionCableSignalingServer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
-      end
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  resources :sessions
-
-  mount ActionCable.server, at: '/cable'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
