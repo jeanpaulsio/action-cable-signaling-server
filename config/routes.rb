5e80dcc62c6245a8e6118adbec6e333ee34b3c6c
JP Sio <sio.jeanpaul@gmail.com>
2020-04-02 21:30:28 -0700

    Revert some things

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  resources :sessions
+
+  mount ActionCable.server, at: '/cable'
 end
